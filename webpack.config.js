448848c2ff18ca1cd7cb2553bbb931a0e5d5202b
annamat <anna.matveev@gmail.com>
2018-01-07 14:52:23 +0200

    Adding eslint & react packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-var HtmlwebpackPlugin = require('html-webpack-plugin');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './app/index.js',
-    output: {
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new HtmlwebpackPlugin({
-            title: 'Waffle',
-            filename: 'index.html'
-        })
-    ]
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Waffle',
+      filename: 'index.html',
+    }),
+  ],
 };

1deb4925b02370b20b58c1609cb2b2d632eae8d0
annamat <anna.matveev@gmail.com>
2018-01-07 14:40:08 +0200

    Creating a skeleton for react app

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlwebpackPlugin({
+            title: 'Waffle',
+            filename: 'index.html'
+        })
+    ]
+};
